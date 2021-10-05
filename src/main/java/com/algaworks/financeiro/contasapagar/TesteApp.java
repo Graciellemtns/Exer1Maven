6a58c75e023e18dbfb91cfa9c626fac2ad14feb5
Gracielle Martins <gracimtns@gmail.com>
2021-10-05 14:06:58 -0300

    Add ex1Maven

---
+++ src/main/java/com/algaworks/financeiro/contasapagar/TesteApp.java
@@ -0,0 +1,12 @@
+package com.algaworks.financeiro.contasapagar;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class TesteApp {
+	public static void main(String[] args) {
+		String nome = "Gracielle Martins";
+				
+				System.out.println(StringUtils.abbreviate(nome, 15));
+	}
+
+}
